b10578ff7c2c78253dbcb6fcb4d4feff6833a9ad
Luis de la Garza <delagarza@gmail.com>
2018-05-17 16:58:34 +0200

    More changes

--- generate.py
+++ generate.py
@@ -3,12 +3,14 @@
 import argparse, shutil, os
 from cookiecutter.main import cookiecutter
 
+# these variables are relative folder paths
+COOKIECUTTERS_ROOT_FOLDER = 'cookiecutters'
 CLI_FOLDER = 'cli'
 GENERIC_LIB_FOLDER = 'generic'
 PORTAL_LIB_FOLDER = 'portal'
 PORTLET_FOLDER = os.path.join('portal', 'portlet')
 
-WORKING_FOLDER = 'cookiecutter_working_folder'
+WORKING_FOLDER = '.cookiecutter_working_folder'
 COMMON_FILES_FOLDER = 'common-files'
 
 def main():
@@ -19,19 +21,19 @@ def main():
     generate_cookiecutter_project(args)
 
 def generate_cookiecutter_project(kwargs):
-    cookiecutter_folder = os.path.join('cookiecutters', {
+    cookiecutter_folder = {
         'cli': CLI_FOLDER,
         'generic-lib': GENERIC_LIB_FOLDER,
         'portal-lib': PORTAL_LIB_FOLDER,
         'portlet': PORTLET_FOLDER
-    }[kwargs.type])
+    }[kwargs.type]
 
-    prepare_cookiecutter_template(cookiecutter_folder)
+    prepare_cookiecutter_template(os.path.join(COOKIECUTTERS_ROOT_FOLDER, cookiecutter_folder))
     cookiecutter(WORKING_FOLDER, no_input=True, overwrite_if_exists=True)
     
 # 
 def prepare_cookiecutter_template(cookiecutter_folder):
-    refresh_working_folder()
+    shutil.rmtree(WORKING_FOLDER, ignore_errors=True)
     copy_cookiecutter_files_to_working_folder(cookiecutter_folder)
     copy_common_files_to_working_folder()
 
@@ -39,18 +41,15 @@ def prepare_cookiecutter_template(cookiecutter_folder):
 def copy_cookiecutter_files_to_working_folder(origin_folder):
     shutil.copytree(origin_folder, WORKING_FOLDER)
 
-# refreshes our working folder (rm -Rf && mkdir)
-def refresh_working_folder():
-    shutil.rmtree(WORKING_FOLDER, ignore_errors=True)
-    os.makedirs(WORKING_FOLDER)
-
 # copies files from the common files folder to the destination folder
 def copy_common_files_to_working_folder():
-    shutil.copytree(COMMON_FILES_FOLDER, WORKING_FOLDER)
-    #common_files = os.listdir(COMMON_FILES_FOLDER)
-    # we ignore log4j configuration, because it needs to be packed in the src/main/resources folder of the working folder
-    #for common_file in common_files:
-    #    shutil.copy(os.path.join(COMMON_FILES_FOLDER, common_file), os.path.join(WORKING_FOLDER, common_file))
+    for item in os.listdir(COMMON_FILES_FOLDER):
+        source = os.path.join(COMMON_FILES_FOLDER, item)
+        dest = os.path.join(WORKING_FOLDER, item)
+        if os.path.isdir(source):
+            shutil.copytree(source, dest)
+        else:
+            shutil.copy2(source, dest)
 
 if __name__ == "__main__":
     main()
