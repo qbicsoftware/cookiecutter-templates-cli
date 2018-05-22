806828b0a34259b59c73bf164bbbf94fe6b74799
Luis de la Garza <delagarza@gmail.com>
2018-05-22 17:16:52 +0200

    Added a logger with two appenders for CLI tools

--- cookiecutters/cli/{{ cookiecutter.artifact_id }}/src/main/java/life/qbic/cli/{{ cookiecutter.main_class }}.java
+++ cookiecutters/cli/{{ cookiecutter.artifact_id }}/src/main/java/life/qbic/cli/{{ cookiecutter.main_class }}.java
@@ -3,6 +3,12 @@ package life.qbic.cli;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
+
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
@@ -11,10 +17,11 @@ import picocli.CommandLine.Option;
  * Tests if an openBIS connection can be established (simple "is it online" tool).
  */
 @Command(
-   name="openBIS connectivity tester",
-   description="This tool is a simple 'is it online' command-line utility.",
-   headerHeading="Usage:%n%n")
+   name="{{ cookiecutter.main_class }}",
+   description="This tool is a simple 'is it online?' command-line utility.")
 public class {{ cookiecutter.main_class }}  {
+
+    private static final Logger LOG = LogManager.getLogger({{ cookiecutter.main_class }}.class);
 
     private static final String VERSION;
     private static final String PROJECT_URL;
@@ -23,41 +30,56 @@ public class {{ cookiecutter.main_class }}  {
         try (final InputStream inputStream = {{ cookiecutter.main_class }}.class.getClassLoader().getResourceAsStream("tool.properties")) {
             properties.load(inputStream);
         } catch (Exception e) {
-            System.err.println("Could not load tools.properties file. Loading default version/project-url.");
-            System.err.println(e);
+            LOG.warn("Could not load tools.properties file. Loading default version/project-url.", e);
         }
         VERSION = properties.getProperty("version", "0.0.1-SNAPSHOT");
         PROJECT_URL = properties.getProperty("project.url", "http://github.com/qbicsoftware");
     }
 
-    @Option(names={"-u", "--url"}, description="openBIS server URL.")
+    @Option(names={"-u", "--url"}, description="openBIS server URL.", required=true)
     private String url;
-    @Option(names={"-n", "--username"}, description="openBIS username.")
+    @Option(names={"-n", "--username"}, description="openBIS username.", required=true)
     private String userName;
-    @Option(names={"-p", "--pass"}, description="openBIS password.")
+    @Option(names={"-p", "--pass"}, description="openBIS password.", required=true)
     private String password;
-    @Option(names={"--version"}, description="Prints version and exits.")
+    @Option(names={"-v", "--version"}, description="Prints version and exits.", versionHelp=true)
     private boolean printVersion;
+    @Option(names={"-h", "--help"}, description="Prints usage and exists.", usageHelp=true)
+    private boolean printHelp;
 
     /**
      * Main method.
      */
     public static void main(final String[] args) {
+        LOG.debug("Starting {{ cookiecutter.main_class }} CLI tool");
         int returnCode = -1;
-        final {{ cookiecutter.main_class }} app = CommandLine.populateCommand(new {{ cookiecutter.main_class }}(), args);
+        try {
+            final {{ cookiecutter.main_class }} app = CommandLine.populateCommand(new {{ cookiecutter.main_class }}(), args);
 
-        if (app.printVersion) {
-            System.out.println(String.format("{{ cookiecutter.main_class }}, version {}", VERSION));
-            returnCode = 0;
-        } else {
-            try {
-                // FIXME: one would typically pack this kind of logic in a library
-                final life.qbic.openbis.openbisclient.OpenBisClient openBisClient = new life.qbic.openbis.openbisclient.OpenBisClient(app.userName, app.password, app.url);
-                openBisClient.login();
+            if (app.printVersion || app.printHelp) {
+                if (app.printVersion) {
+                    LOG.debug("Version requested.");
+                    LOG.info("{{ cookiecutter.main_class }}, version {}", VERSION);
+                }
+                if (app.printHelp) {
+                    LOG.debug("Help requested.");
+                    CommandLine.usage(app, System.out);
+                }
                 returnCode = 0;
-            } catch (Exception e) {
-                System.out.println(String.format("Could not establish connection to openBIS server at {} (username={}, password=(not shown!))", app.url, app.userName));
+            } else {
+                try {
+                    // FIXME: one would typically pack this kind of logic in a library
+                    final life.qbic.openbis.openbisclient.OpenBisClient openBisClient = new life.qbic.openbis.openbisclient.OpenBisClient(app.userName, app.password, app.url);
+                    openBisClient.login();
+                    returnCode = 0;
+                } catch (Exception e) {
+                    LOG.error("Could not establish connection to openBIS server at {} (username={}, password=(****))", app.url, app.userName);
+                }
             }
+        } catch (Exception e) {
+            LOG.error(e.getMessage());
+            LOG.error("Check the application log in logs/app.log for more details.");
+            LOG.debug("Full stack trace: ", e);
         }
         System.exit(returnCode);
     }
--- generate.py
+++ generate.py
@@ -13,6 +13,7 @@ PORTAL_LIB_FOLDER = 'portal'
 PORTLET_FOLDER = 'portlet'
 WORKING_FOLDER = '.cookiecutter_working_folder'
 COMMON_FILES_FOLDER = 'common-files'
+DEFAULT_OUTPUT_DIR = 'generated'
 
 # parses arguments
 def main():
@@ -20,9 +21,11 @@ def main():
     parser.add_argument('-t', '--type', choices=['cli', 'generic-lib', 'portal-lib', 'portlet'], required=True, 
         help='The type of artifact you want to generate (i.e., generic Java library, portal library, CLI tool or portlet).')
     parser.add_argument('--no-input', action='store_true', 
-        help='If set, default values, as defined in common-files/cookiecutter.json, will be used and no prompt will be displayed.')
+        help='If set, default values, as defined in the correspoing cookiecutter.json, will be used and no prompt will be displayed. There is one cookiecutter.json file associated with each type (e.g., cli/cookiecutter.json, portal/cookiecutter.json)')
     parser.add_argument('extra_context', metavar='extra_context', nargs='*', 
         help='List of variables/values that will override cookiecutter defaults (see cookiecutter documentation for a thorough explanation). Format: var1=val1 var2=val2 ... varN=valN')
+    parser.add_argument('-o', '--output-dir', default=DEFAULT_OUTPUT_DIR,
+        help='Specifies the output folder of generated projects.')
     args = parser.parse_args()
 
     args.extra_context = validate_extra_context(None, None, args.extra_context)
@@ -38,13 +41,13 @@ def generate_cookiecutter_project(kwargs):
     }[kwargs.type]
 
     prepare_cookiecutter_template(os.path.join(COOKIECUTTERS_ROOT_FOLDER, cookiecutter_folder))
-    cookiecutter(WORKING_FOLDER, no_input=kwargs.no_input, overwrite_if_exists=True, extra_context=kwargs.extra_context)
+    cookiecutter(WORKING_FOLDER, no_input=kwargs.no_input, overwrite_if_exists=True, extra_context=kwargs.extra_context, output_dir=kwargs.output_dir)
     
 # removes any lingering working folder, copies content from the desired cookecutter folder (e.g., portal/portlet)
 def prepare_cookiecutter_template(cookiecutter_folder):
     shutil.rmtree(WORKING_FOLDER, ignore_errors=True)
-    copy_cookiecutter_files_to_working_folder(cookiecutter_folder)
     copy_common_files_to_working_folder()
+    copy_cookiecutter_files_to_working_folder(cookiecutter_folder)
 
 # copies files from one of the cookiecutter folders (e.g., portal/portlet) to our working directory
 def copy_cookiecutter_files_to_working_folder(origin_folder):
