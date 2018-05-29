0bbad58c5a1af8e49d14297fb03da09f22a22b01
Luis de la Garza <delagarza@gmail.com>
2018-05-29 12:46:53 +0200

    Using positional parameters at the very end

--- .travis.sh
+++ .travis.sh
@@ -3,7 +3,8 @@
 # we override the artifact_id variable; the tests in (test_template.py) depend on this value, so be careful when you change it
 
 # create all projects (just four, for now... maybe later we need to do this using for loops)
-rm -Rf template-test && ./generate.py --no-input --output-dir . artifact_id=template-test --type=$TYPE \
+rm -Rf template-test && ./generate.py --no-input --output-dir . --type=$TYPE \
+                artifact_id=template-test \
                 use_openbis_client=$USE_OPENBIS_CLIENT \
                 use_openbis_raw_api=$USE_OPENBIS_CLIENT \
                 use_qbic_databases=$USE_QBIC_DATABASES \
