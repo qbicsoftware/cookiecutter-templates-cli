ef1a0fa2f7014334e6f4387d205b5f376a855c07
Luis de la Garza <delagarza@gmail.com>
2018-07-03 15:54:03 +0200

    Removed compiled widgetsets

--- sample-code/portlet/src/main/webapp/VAADIN/widgetsets/life.qbic.portal.portlet.AppWidgetSet/deferredjs/B85C5813BB5D01B0594728EE577221E9/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.life_qbic_portal_portlet_AppWidgetSet.runAsyncCallback2('seb(1626,1,q4d);_.vc=function Cjc(){c4b((!X3b&&(X3b=new h4b),X3b),this.a.d)};NZd(Th)(2);\n//# sourceURL=life.qbic.portal.portlet.AppWidgetSet-2.js\n')
