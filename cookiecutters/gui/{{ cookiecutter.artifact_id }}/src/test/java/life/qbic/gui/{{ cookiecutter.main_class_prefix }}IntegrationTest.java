e477474e041c17b98fb3ca73eb6922c8cd21902e
Luis de la Garza <delagarza@gmail.com>
2019-03-26 18:02:49 -0600

    Using version 1.4.0 of the parent POMs; added integration tests to cookiecutters (#32)
    Fixes #31

---
+++ cookiecutters/gui/{{ cookiecutter.artifact_id }}/src/test/java/life/qbic/gui/{{ cookiecutter.main_class_prefix }}IntegrationTest.java
@@ -0,0 +1,14 @@
+package life.qbic.gui;
+
+import org.junit.Test;
+
+/**
+ * Integration tests for {{ cookiecutter.main_class_prefix }}Application.
+ */
+public class {{ cookiecutter.main_class_prefix }}IntegrationTest  {
+    // TODO: write unit tests (you do not need to test JavaFXExecutor, just test start(Stage) method of your application)
+
+    @Test
+    public void testToolConnectsToTheCloud() {
+    }
+}
